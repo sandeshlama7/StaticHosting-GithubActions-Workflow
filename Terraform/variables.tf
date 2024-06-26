3eeff4c8f9e5b44ec258e6b863fc725bffd46fe1
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-06-26 13:09:54 +0545

    Added Terraform Code for EC2, SG and IAM-role

---
+++ Terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  description = "Instance type to use for the EC2"
+  type = string
+}
+variable "names" {
+  description = "Names for different resources"
+  type = map(string)
+}
+variable "subnet_id" {
+  description = "Subnet ID of poc us-east-1f public subnet"
+  type = string
+}
