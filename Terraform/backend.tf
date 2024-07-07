0ffbdd2908f9afcf0feb02d03c0bdc0086c892d3
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-07-07 14:05:04 +0545

    Passing tf backend info from the workflow

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "8586-terraform-state"
-    key    = "sandesh/terraform.tfstate"
-    region = "us-east-1"
+    # bucket = "8586-terraform-state"
+    # key    = "sandesh/terraform.tfstate"
+    # region = "us-east-1"
   }
 }
